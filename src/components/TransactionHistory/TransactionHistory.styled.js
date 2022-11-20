088062903b957e2db570862e2b06ea79e6f1134f
Deniss Simonaits <dsimonaits@gmail.com>
2022-11-20 21:25:24 +0200

    added component transaction

---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+  width: 400px;
+
+  border: 1px solid grey;
+  border-collapse: collapse;
+
+  td {
+    text-align: center;
+    line-height: 3;
+    border: 1px solid grey;
+  }
+
+  tbody tr {
+    :nth-of-type(odd) {
+      background-color: #efefef;
+    }
+    :hover {
+      background-color: lightsteelblue;
+    }
+  }
+  thead > tr {
+    line-height: 3;
+    background-color: navajowhite;
+  }
+
+  thead th {
+    border: 1px solid grey;
+  }
+`;
